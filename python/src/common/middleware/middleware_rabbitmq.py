62c59672f3b03f64ed53a45d0c9097271465492b
Lucia <lucia.liceri@gmail.com>
2026-09-15 16:51:38 -0300

    Hello World, first steps. Undestanding the code.

--- python/src/common/middleware/middleware_rabbitmq.py
+++ python/src/common/middleware/middleware_rabbitmq.py
@@ -6,9 +6,17 @@ from .middleware import MessageMiddlewareQueue, MessageMiddlewareExchange
 class MessageMiddlewareQueueRabbitMQ(MessageMiddlewareQueue):
 
     def __init__(self, host, queue_name):
-        pass
+        self.connection = pika.BlockingConnection(pika.ConnectionParameters(host))
+        self.channel = connection.channel()
+        # Queue declaration
+        channel.queue_declare(queue=queue_name)
+        channel.basic_publish(exchange='', routing_key=queue_name, body=message)
+        
 
 class MessageMiddlewareExchangeRabbitMQ(MessageMiddlewareExchange):
     
     def __init__(self, host, exchange_name, routing_keys):
-        pass
+        self.connection = pika.BlockingConnection(pika.ConnectionParameters(host))
+        self.channel = connection.channel()
+        channel.basic_publish(exchange=exchange_name, routing_key=routing_key, body=message)
+        
